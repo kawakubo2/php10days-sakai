9fc315ff4cbf28903d8a71367745f4901031c019
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-05-28 17:33:08 +0900

    ?

---
+++ lesson04/begin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+print('こんにちは、世界！<br>');
+print('こんにちは、PHP！<br>');
+echo 'こんにちは、HTML！<br>';
+print 'こんにちは、CSS！<br>';
+?>
+</body>
+</html>
