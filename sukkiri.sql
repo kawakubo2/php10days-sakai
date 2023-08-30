392f91acb9ff3f6c1620509bc8da1bbab5860cb8
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-08-30 13:59:08 +0900

    2023年08月30日授業分

--- sukkiri.sql
+++ sukkiri.sql
@@ -99,3 +99,37 @@ CHARACTER SET sjis
 LINES TERMINATED BY '\r\n';
 
 SELECT * FROM 都道府県;
+
+INSERT INTO 都道府県(コード, 地域, 都道府県名, 面積)
+VALUES('26', '近畿', '京都', 4613);
+
+INSERT INTO 都道府県
+VALUES('37', '四国', '香川', '高松', 1876);
+
+INSERT INTO 都道府県(コード, 都道府県名, 県庁所在地)
+VALUES('40', '福岡', '福岡');
+
+UPDATE 都道府県
+SET 県庁所在地 = '京都'
+WHERE コード = '26';
+
+UPDATE 都道府県
+SET 地域 = '九州', 面積 = 4976
+WHERE コード = '40';
+
+DELETE FROM 都道府県
+WHERE コード = '26';
+
+SELECT *
+FROM 家計簿
+WHERE 入金額 IS NULL;
+
+SELECT *
+FROM 家計簿
+WHERE メモ IS NOT NULL;
+
+SHOW TABLES;
+
+SELECT *
+FROM 家計簿
+WHERE メモ LIKE '%1月%';
--- 入門SQL坂井.sql
+++ 入門SQL坂井.sql
@@ -1,1 +1,40 @@
 show tables;
+
+SELECT *
+FROM 書籍;
+/*
+前方一致検索はパフォーマンスに影響なし
+部分一致検索と後方一致検索は大量のデータを
+検索する場合、著しくパフォーマンスが
+低下する
+*/
+-- 部分一致検索
+SELECT *
+FROM 書籍
+WHERE シリーズ名 LIKE '%入門%';
+
+-- 前方一致検索
+SELECT *
+FROM 書籍
+WHERE シリーズ名 LIKE '入門%';
+
+-- 後方一致検索
+SELECT *
+FROM 書籍
+WHERE シリーズ名 LIKE '%入門';
+
+SELECT *
+FROM 書籍
+WHERE 分類 LIKE 'jp%';
+
+SELECT *
+FROM 書籍
+WHERE 分類 LIKE binary 'jp%';
+
+SELECT *
+FROM 書籍
+WHERE 分類 LIKE 'EU_';
+
+SELECT *
+FROM 書籍
+WHERE 書籍コード LIKE '1___2';
